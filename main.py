c4563cf12d409a79e00c587bb42832f63780788e
Nick <83893877+MortyHub@users.noreply.github.com>
2022-01-31 15:00:56 +0000

    Name Creator

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ window.title("Notepad")
 def Savee():
     textt = Tet.get("1.0", "end-1c")
     extension = fileEx.get("1.0", "end-1c")
+    name = Name.get("1.0", "end-1c")
 
     try:
-        with open(f"default.{extension}", "x") as f:
+        with open(f"{name}.{extension}", "x") as f:
             f.write(textt)
     except:
-        with open(f"default({str(random.randrange(1,1000))}).{extension}", "x") as f:
+        with open(f"{name}({str(random.randrange(1,1000))}).{extension}", "x") as f:
             f.write(textt)
     
 
 Tet = Text(window, height=200, width=200, font=("Times New Roman", 10))
-Tet.place(x=0,y=50)
+Tet.place(x=0,y=55)
+
 
 Save = Button(window, text="Save As", fg="Red", font=("Times New Roman", 15), command=Savee)
 Save.place(x=0, y=0)
 
 fileEx = Text(window, height=1, width=25, font=("Times New Roman", 14))
-fileEx.place(x=180,y=10)
+fileEx.place(x=180,y=0)
+
+Name = Text(window, height=1, width=25, font=("Times New Roman", 14))
+Name.place(x=180, y=25)
 
 window.attributes('-fullscreen', True)
 window.mainloop()
