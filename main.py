513ddec080b34b0c6bc81c365c58d452190e0dca
Nick <83893877+MortyHub@users.noreply.github.com>
2022-01-31 14:17:29 +0000

    Button Fix

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def Savee():
 Tet = Text(window, height=200, width=200, font=("Times New Roman", 10))
 Tet.place(x=0,y=50)
 
-Save = Button(window, text="Save As", fg="Red", font=("Times New Roman", 24), command=Savee)
+Save = Button(window, text="Save As", fg="Red", font=("Times New Roman", 15), command=Savee)
 Save.place(x=0, y=0)
 
 fileEx = Text(window, height=1, width=25, font=("Times New Roman", 14))
